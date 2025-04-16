b25c6358a8bd8e178b3652956d8df8b55bf843ff
KashiRam207 <justlucky207@gmail.com>
2025-04-16 14:36:14 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,10 +4,10 @@ region = "us-east-1"
 
 resource "aws_instance" "one" {
 count = 4
-ami = "ami-04adc082cd8225313"
+ami = "ami-07a6f770277670015"
 instance_type = "t2.medium"
 key_name = "sarikey1"
-vpc_security_group_ids = ["sg-00281e35874780e16"]
+vpc_security_group_ids = ["sg-0b0d0cb23f39a465b"]
 tags = {
 Name = var.instance_names[count.index]
 }
